18a8814975f84b4ce4e285c9ea358eaef55a8a97
Róman Demchuk <roman.demchuk@gmail.com>
2023-03-06 21:30:39 +0100

    Add allowed origins to cors options

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,3 +1,4 @@
+import "../loadEnvironments.js";
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
@@ -6,6 +7,13 @@ export const app = express();
 
 app.disable("x-powered-by");
 
+const allowedOrigins = [
+  process.env.CORS_ALLOWED_LOCAL!,
+  process.env.CORS_ALLOWED_PRODUCTION!,
+];
+
+const corsOptions: cors.CorsOptions = { origin: allowedOrigins };
+
+app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(cors());
