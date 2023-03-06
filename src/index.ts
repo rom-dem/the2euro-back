7bd7b8dbf48d8762d1109d3823f17d04d590d63b
Róman Demchuk <roman.demchuk@gmail.com>
2023-03-06 20:22:12 +0100

    Add connectDatabase to index.Use loadEnvronments

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-const a = 1;
+import "./loadEnvironments.js";
+import mongoose from "mongoose";
+import connectDatabase from "./database/connectDatabase.js";
+
+const port = process.env.PORT ?? 4000;
+const databaseUrl = process.env.MONGODB_CONNECTION_URL!;
+
+await connectDatabase(databaseUrl);
