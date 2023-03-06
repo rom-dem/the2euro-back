13624511df75cd5e4091cb79492e8e15d657077e
Róman Demchuk <roman.demchuk@gmail.com>
2023-03-06 20:25:24 +0100

    Add mongoose virtuals for _id and __v

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,12 @@ import connectDatabase from "./database/connectDatabase.js";
 const port = process.env.PORT ?? 4000;
 const databaseUrl = process.env.MONGODB_CONNECTION_URL!;
 
+mongoose.set("toJSON", {
+  virtuals: true,
+  transform(doc, ret) {
+    delete ret._id;
+    delete ret.__v;
+  },
+});
+
 await connectDatabase(databaseUrl);
