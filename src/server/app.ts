f20022e02f8a5d53c38ec06119801d7302b6f4bc
Róman Demchuk <roman.demchuk@gmail.com>
2023-03-06 21:11:50 +0100

    Add initial app server set up (express,cors,morgan)

---
+++ src/server/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+export const app = express();
+
+app.disable("x-powered-by");
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
