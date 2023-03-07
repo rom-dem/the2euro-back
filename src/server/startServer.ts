5c1c3dbb18111118b4163904c93c6ed5859ec829
Róman Demchuk <roman.demchuk@gmail.com>
2023-03-07 11:44:57 +0100

    Create CustomError and add it to startServer

--- src/server/startServer.ts
+++ src/server/startServer.ts
@@ -1,6 +1,7 @@
 import "../loadEnvironments.js";
 import createDebug from "debug";
 import { app } from "./app.js";
+import { type CustomError } from "../CustomError/CustomError.js";
 
 const debug = createDebug("the2euro:server:startServer");
 
@@ -10,8 +11,13 @@ const startServer = async (port: number) =>
       resolve(server);
       debug(`Server is listening on http://localhost:${port}`);
     });
-    server.on("error", (error: Error) => {
+    server.on("error", (error: CustomError) => {
       const errorMessage = "Error starting the server";
+
+      if (error.code === "EADDRINUSE") {
+        debug(errorMessage, `The port ${port} is already in use`);
+      }
+
       reject(new Error(errorMessage));
     });
   });
