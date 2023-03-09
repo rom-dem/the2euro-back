0386481441b5ab46e2be0cc23b671010c4c5cab0
Róman Demchuk <roman.demchuk@gmail.com>
2023-03-09 10:34:27 +0100

    Manage error in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import "./loadEnvironments.js";
 import mongoose from "mongoose";
 import connectDatabase from "./database/connectDatabase.js";
 import startServer from "./server/startServer.js";
+import createDebug from "debug";
+
+const debug = createDebug("the2euro");
 
 const port = process.env.PORT ?? 4000;
 const databaseUrl = process.env.MONGODB_CONNECTION_URL!;
@@ -14,5 +17,9 @@ mongoose.set("toJSON", {
   },
 });
 
-await connectDatabase(databaseUrl);
-await startServer(+port);
+try {
+  await connectDatabase(databaseUrl);
+  await startServer(+port);
+} catch (error) {
+  debug(error);
+}
