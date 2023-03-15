4a4573fcb40578de03111784e119f2685f3ace78
Róman Demchuk <roman.demchuk@gmail.com>
2023-03-15 19:40:26 +0100

    Add deleteCoin to coinControllers

--- src/server/controllers/coinControllers/coinControllers.ts
+++ src/server/controllers/coinControllers/coinControllers.ts
@@ -1,6 +1,7 @@
 import { type NextFunction, type Request, type Response } from "express";
 import { CustomError } from "../../../CustomError/CustomError.js";
 import Coin from "../../../database/models/Coin/Coin.js";
+import { type CustomRequest } from "../../../types/users/types.js";
 
 export const getCoins = async (
   req: Request,
@@ -10,14 +11,43 @@ export const getCoins = async (
   try {
     const coins = await Coin.find().exec();
 
-    res.status(200).json({ coins });
-  } catch (error: unknown) {
-    const customError = new CustomError(
-      (error as Error).message,
-      500,
-      "Couldn't find coins"
-    );
+    if (!coins) {
+      throw new CustomError(
+        "Internal server error",
+        500,
+        "Couldn't find coins"
+      );
+    }
 
-    next(customError);
+    res.status(200).json({ coins });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteCoin = async (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const { coinId } = req.params;
+
+  try {
+    const coin = await Coin.findByIdAndDelete({
+      _id: coinId,
+      owner: req.owner,
+    }).exec();
+
+    if (!coin) {
+      throw new CustomError(
+        "Internal server error",
+        500,
+        "Couldn't delete the coin"
+      );
+    }
+
+    res.status(200).json({ coin });
+  } catch (error) {
+    next(error);
   }
 };
